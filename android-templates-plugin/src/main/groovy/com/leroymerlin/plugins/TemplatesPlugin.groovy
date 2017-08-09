8db5892aa0036621bf74d8bd338eadfe6e29c1c9
paillardf <paillard.f@gmail.com>
2017-08-09 12:02:43 +0200

    feat (init) : first commit

---
+++ android-templates-plugin/src/main/groovy/com/leroymerlin/plugins/TemplatesPlugin.groovy
@@ -0,0 +1,15 @@
+package com.leroymerlin.plugins.templates
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * Created by florian on 04/07/2017.
+ */
+class TemplatesPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+    }
+}
